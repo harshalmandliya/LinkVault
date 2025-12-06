a10a340e39a9538c3998106c39d64415ce0c5afc
harshalmandliya <harshalmandliya3@gmail.com>
2025-12-06 12:17:55 +0530

    Implemented URL Redirect Functionality

--- Backend/src/main/java/com/url/shortner/service/UrlMappingService.java
+++ Backend/src/main/java/com/url/shortner/service/UrlMappingService.java
@@ -87,6 +87,21 @@ public class UrlMappingService {
 		List<ClickEvent> clickEvents=clickEventRepository.findByUrlMappingInAndClickDateBetween(urlMappings, start.atStartOfDay(), end.plusDays(1).atStartOfDay());
 		return clickEvents.stream().collect(Collectors.groupingBy(click->click.getClickDate().toLocalDate(),Collectors.counting()));
 	}
+
+	public UrlMapping getOriginalUrl(String shortUrl) {
+		UrlMapping urlMapping=urlMappingRepository.findByShortUrl(shortUrl);
+		if(urlMapping!=null) {
+			urlMapping.setClickCount(urlMapping.getClickCount()+1);
+			urlMappingRepository.save(urlMapping);
+			
+			ClickEvent clickEvent=new ClickEvent();
+			clickEvent.setClickDate(LocalDateTime.now());
+			clickEvent.setUrlMapping(urlMapping);
+			clickEventRepository.save(clickEvent);
+		}
+		
+		return urlMapping;
+	}
 	
 
 }
