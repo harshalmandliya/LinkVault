934bd3310e1262535bfeffb6a07c8f6de8fc2643
harshalmandliya <harshalmandliya3@gmail.com>
2026-04-27 00:28:04 +0530

    feat: implement landing page, authentication (login/signup), and configure CORS handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,25 @@ import { BrowserRouter } from "react-router-dom"
 import { Routes, Route } from "react-router-dom"
 import LandingPage from "./components/LandingPage"
 import AboutPage from "./components/AboutPage"
+import Footer from "./components/Footer"
+import NavBar from "./components/NavBar"
+import RegisterPage from "./components/RegisterPage"
+import { Toaster } from "react-hot-toast";
+import LoginPage  from "./components/LoginPage"
 
 function App() {
   return (
      
     <BrowserRouter>
+    <NavBar/>
+    <Toaster position='bottom-center'/>
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/about" element={<AboutPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<LoginPage/>}/>
     </Routes>
+    <Footer/>
     </BrowserRouter>
     )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { ContextProvider } from './contextApi/ContextApi.jsx';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </StrictMode>,
 )
